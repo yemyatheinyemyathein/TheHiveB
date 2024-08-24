cbb7dccd34299d77ffb062f186bd1db746ef7138
Ye Myat Hein <yemyathein67@gmail.com>
2024-08-25 03:02:43 +0630

    fix: something

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const connectDB = require("./util/db");
 const cors = require('cors');
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+//   }));
+
+app.use(express.json());
 
 
 app.use(express.json({ limit: '10mb' }));
@@ -22,7 +26,7 @@ app.use("/", (req, res, next) => {
 });
 
 app.use(UserRoutes);
-app.use('/admin', cors(corsOptions), AdminRoutes);
+app.use('/admin',AdminRoutes);
 
 connectDB().then(() => {
     app.listen(process.env.PORT, () => {
